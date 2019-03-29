b64669c4c1fa3c1693a8561a5bbacf914cc28b1a
RZ <resizoltan@live.com>
2019-03-29 01:55:47 +0100

    peripheral and port base class created

---
+++ PortMediator/Peripheral.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortMediator
+{
+    abstract class Peripheral
+    {
+        bool isOpen;
+        bool canSend;
+        bool canReceive;
+        public string id { get; }
+
+        List<Port> availablePorts;
+
+        public abstract Task<bool> StartPeripheral();
+        public abstract Task<bool> StopPeripheral();
+
+        public event EventHandler<PortReceivedEventArgs> PortReceived;
+
+    }
+
+
+
+    public class PortReceivedEventArgs : EventArgs
+    {
+        public Port port { get; set; }
+    }
+}
--- PortMediator/Port.cs
+++ PortMediator/Port.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace PortMediator
 {
-    abstract class Port
+    abstract public class Port
     {
+        int type;
+        bool isOpen;
+        bool canSend;
+        bool canReceive;
+
         public event EventHandler<DataReceivedEventArgs> DataReceived;
+
+        public abstract Task<bool> OpenPort();
+        public abstract void ClosePort();
+        public abstract Task<bool> StartReadingPort();
+        public abstract void StopReadingPort();
+
         public abstract void SendData(byte[] data);
         protected void OnDataReceived(byte[] data)
         {
@@ -20,19 +31,18 @@ namespace PortMediator
                 handler(this, args);
             }
         }
-        public abstract Task<bool> OpenPort();
-        public abstract void ClosePort();
-        public abstract Task<bool> StartReading();
+
+        public class DataReceivedEventArgs : EventArgs
+        {
+            public Port port { get; set; }
+            public byte[] data { get; set; }
+        }
+
+        public int Identify(byte[] fromData)
+        {
+            new NotImplementedException();
+            return 0;
+        }
     }
 
-    public class DataReceivedEventArgs : EventArgs
-    {
-        public byte[] data { get; set; }
-
-    }
-
-    class DataProcessor
-    {
-
-    }
 }
