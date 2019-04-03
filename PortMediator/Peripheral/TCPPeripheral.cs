a9dc6f0efba6646d7d7cc3aa6550463cc62e8712
RZ <resizoltan@live.com>
2019-04-04 01:19:39 +0200

    tcpperipheral constructor added

--- PortMediator/Peripheral/TCPPeripheral.cs
+++ PortMediator/Peripheral/TCPPeripheral.cs
@@ -10,19 +10,20 @@ using System.Net.Sockets;
 
 namespace PortMediator
 {
-    class TCPPeripheral : Peripheral
+    class TCPPort : Port
     {
-        const int localPortNumber = 11000;
-        TcpListener tcpListener = null;
 
+        public TCPPort(TcpClient tcpClient)
+        {
 
+        }
 
-        public override Task<bool> Start()
+        public override string GetID()
         {
             throw new NotImplementedException();
         }
 
-        public override Task<bool> Stop()
+        public override void Open(Peripheral serialPeripheral)
         {
             throw new NotImplementedException();
         }
@@ -31,5 +32,78 @@ namespace PortMediator
         {
             throw new NotImplementedException();
         }
+
+        public override void StartReading()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StartWaitingForConnectionRequest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task SendData(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StopReading(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class TCPPeripheral : Peripheral
+    {
+        const int localPortNumber = 11000;
+        IPEndPoint localEndPoint = null;
+        TcpListener tcpListener = null;
+
+        CancellationTokenSource acceptTcpClientTaskCTS = new CancellationTokenSource();
+
+        public TCPPeripheral()
+        {
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress localIPAddress = ipHostInfo.AddressList.Single(
+                    ipAddress => ipAddress.AddressFamily == AddressFamily.InterNetwork);
+                localEndPoint = new IPEndPoint(localIPAddress, localPortNumber);
+
+                tcpListener = new TcpListener(localEndPoint);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error occured in TCPPeripheral() of " + localEndPoint.ToString());
+                Console.WriteLine("\tError source: " + e.Source);
+                Console.WriteLine("\tError message: " + e.Message);
+            }
+        }
+
+        public override void Start()
+        {
+            tcpListener.Start();
+            CancellationToken acceptTcpClientTaskCT = acceptTcpClientTaskCTS.Token;
+            Task.Factory.StartNew(delegate
+            {
+                while (!acceptTcpClientTaskCTS.IsCancellationRequested)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    TCPPort tcpPort = new TCPPort(tcpClient);
+                    ports.Add(tcpPort);
+                }
+            }, acceptTcpClientTaskCT, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        public override void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public override void Close()
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
