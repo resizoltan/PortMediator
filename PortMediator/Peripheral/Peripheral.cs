4f14cb751131aa5f9fc0344b2129e2d07aca55a4
RZ <resizoltan@live.com>
2019-04-05 17:05:07 +0200

    Port events created apart from error event

--- PortMediator/Peripheral/Peripheral.cs
+++ PortMediator/Peripheral/Peripheral.cs
@@ -11,8 +11,8 @@ namespace PortMediator
     {
         protected const int connectionRequestMessageLength = 3;
 
-        protected Action<Client> NewClientHandler = null;
-
+        //protected Action<Client> NewClientHandler = null;
+        
         protected Task readTask = null;
         protected Task sendTask = null;
         protected Task waitForClientConnectionTask = null;
@@ -27,6 +27,23 @@ namespace PortMediator
             get { return waitForClientConnectionTask; }
         }
 
+        protected CancellationTokenSource readTaskCTS = new CancellationTokenSource();
+        protected CancellationTokenSource sendTaskCTS = new CancellationTokenSource();
+        protected CancellationTokenSource waitForConnectionRequestTaskCTS = new CancellationTokenSource();
+
+        public event EventHandler<BytesReceivedEventArgs> DataReceived;
+        public event EventHandler<NewClientEventArgs> NewClientConnected;
+        public event EventHandler<PortClosedEventArgs> PortClosed;
+
+        public abstract void Open();
+        public abstract void Close();
+        public abstract void StartReading();
+        public abstract void StartWaitingForConnectionRequest();
+        public abstract void StopReading(Client client);
+        public abstract void SendData(byte[] data);
+
+        public abstract string ID { get; }
+
         public async Task WaitForAllOperationsToComplete()
         {
             try
@@ -39,75 +56,46 @@ namespace PortMediator
 
         }
 
-        protected CancellationTokenSource readTaskCTS = new CancellationTokenSource();
-        protected CancellationTokenSource sendTaskCTS = new CancellationTokenSource();
-        protected CancellationTokenSource waitForConnectionRequestTaskCTS = new CancellationTokenSource();
-
-        public Port(Action<Client> NewClientHandler)
-        {
-            this.NewClientHandler = NewClientHandler;
-        }
-        public abstract void Open();
-        public abstract void Close();
-        public abstract void StartReading();
-        public abstract void StartWaitingForConnectionRequest();
-        public abstract void StopReading(Client client);
-        public abstract string ID { get; }
-
-        public abstract void SendData(byte[] data);
-
-        public event EventHandler<BytesReceivedEventArgs> DataReceived;
-
-        public void OnDataReceived(byte[] data)
+        protected void OnDataReceived(BytesReceivedEventArgs eventArgs)
         {
             EventHandler<BytesReceivedEventArgs> handler = DataReceived;
-            if (handler != null)
-            {
-                BytesReceivedEventArgs args = new BytesReceivedEventArgs();
-                args.data = data;
-                handler(this, args);
-            }
+            handler?.Invoke(this, eventArgs);
         }
-
-        //public void OnClose(string reason)
-        //{
-        //    EventHandler<CloseEventArgs> handler = Closes;
-        //    if (handler != null)
-        //    {
-        //        CloseEventArgs args = new CloseEventArgs();
-        //        args.reason = reason;
-        //        handler(this, args);
-        //    }
-        //}
-
-        //public class CloseEventArgs : EventArgs
-        //{
-        //    public string reason { get; set; } = "unknown";
-        //}
-
-        public void ConnectionRequested(Port onPort, byte[] data)
+        protected void OnClose(PortClosedEventArgs eventArgs)
         {
-            if(data.Length == connectionRequestMessageLength)
+            EventHandler<PortClosedEventArgs> handler = PortClosed;
+            handler?.Invoke(this, eventArgs);
+        }
+        protected void OnConnectionRequest(ConnectionRequestEventArgs eventArgs)
+        {
+            byte[] bytes = eventArgs.bytes;
+            if(bytes != null && bytes.Length == connectionRequestMessageLength)
             {
                 try
                 {
-                    Client.TYPE type = Client.Identify((byte)(data[0]));
-                    string name = Client.typenames[type] + "_" + Encoding.ASCII.GetString(data, 1, 2);
+                    Client.TYPE type = Client.Identify((byte)(bytes[0]));
+                    string name = Client.typenames[type] + "_" + Encoding.ASCII.GetString(bytes, 1, 2);
                     Client newClient = Client.CreateNew(type, name, this);
-                    NewClientHandler(newClient);
+                    NewClientEventArgs newClientEventArgs = new NewClientEventArgs(newClient);
+                    OnNewClient(newClientEventArgs);
                 }
                 catch(Exception e)
                 {
                     Console.WriteLine("Error occured in Port.ConnectionRequested()");
                     Console.WriteLine("Error source:  " + e.Source);
                     Console.WriteLine("Error message: " + e.Message);
-                    onPort.StartWaitingForConnectionRequest();
+                    this.StartWaitingForConnectionRequest();
                 }
             }
             else
             {
-                onPort.StartWaitingForConnectionRequest();
+                this.StartWaitingForConnectionRequest();
             }
+        }
+        protected void OnNewClient(NewClientEventArgs eventArgs)
+        {
+            EventHandler<NewClientEventArgs> handler = NewClientConnected;
+            handler?.Invoke(this, eventArgs);
         }
     }
 
@@ -155,19 +143,48 @@ namespace PortMediator
         //}
     }
 
+    public class ConnectionRequestEventArgs : EventArgs
+    {
+        public byte[] bytes { get; set; }
+        public ConnectionRequestEventArgs(byte[] bytes)
+        {
+            this.bytes = bytes;
+        }
+    }
+
     public class NewClientEventArgs : EventArgs
     {
         public Client client { get; set; }
+        public NewClientEventArgs(Client client)
+        {
+            this.client = client;
+        }
     }
 
     public class PacketReceivedEventArgs : EventArgs
     {
         public Communication.Packet packet { get; set; }
+        public PacketReceivedEventArgs(Communication.Packet packet)
+        {
+            this.packet = packet;
+        }
     }
 
     public class BytesReceivedEventArgs : EventArgs
     {
-        public byte[] data { get; set; }
+        public byte[] bytes { get; set; }
+        public BytesReceivedEventArgs(byte[] bytes)
+        {
+            this.bytes = bytes;
+        }
     }
 
+    public class PortClosedEventArgs : EventArgs
+    {
+        public string initiator { get; set; }
+        public PortClosedEventArgs(string initiator)
+        {
+            this.initiator = initiator;
+        }
+    }
 }
